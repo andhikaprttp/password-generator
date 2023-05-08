eb7fb4bb36c212f477510339b93cc9adc4ed3fbd
andhika pratama putra <120385412+andhikaprttp@users.noreply.github.com>
2023-05-08 21:18:55 +0700

    update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-    function generatePassword(length) {
-        var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+{}[]:;<>,.?/~`π÷'×§π";
-        var password = "";
-        for (var i = 0; i < length; i++) {
-          var randomIndex = Math.floor(Math.random() * charset.length);
-          password += charset[randomIndex];
-        }
-        return password;
-      }
+function generatePassword(length) {
+  var charset =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+{}[]:;<>,.?/~`π÷'×§π";
+  var password = "";
+  for (var i = 0; i < length; i++) {
+    var randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset[randomIndex];
+  }
+  return password;
+}
 
-      var generateButton = document.getElementById("generate-button");
-      generateButton.addEventListener("click", function() {
-        var passwordLength = document.getElementById("password-length").value;
-        var passwordDisplay = document.getElementById("password-display");
-        passwordDisplay.innerHTML = generatePassword(passwordLength);
-      });
-   
+var generateButton = document.getElementById("generate-button");
+generateButton.addEventListener("click", function () {
+  var passwordLength = document.getElementById("password-length").value;
+  var passwordDisplay = document.getElementById("password-display");
+  passwordDisplay.innerHTML = generatePassword(passwordLength);
+});
